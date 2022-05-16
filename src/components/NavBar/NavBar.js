49e47f9fcca4a3de2ba48820f575b6d6f9f0e09d
Willowskyz <guillermompm@gmail.com>
2022-05-16 20:21:39 -0300

    Prueba con Manuel NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Tienda Willowsky</h1>
+            <div>
+                <button>Cafe Colombiano</button>
+                <button>Cafe Peruano</button>
+                <button>Cafe Brasilero</button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default NavBar
