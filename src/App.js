71426e4a795eadd21d33784d173c76f611a53c71
Willowskyz <guillermompm@gmail.com>
2022-05-18 12:43:40 -0300

    Desafio componentes 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
 import NavBar from './components/NavBar/NavBar.js';
+import CountFunction from './components/CountFunction/CountFunction.js';
+
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'; /* Solo para el desafio */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <CountFunction />
+        <ItemListContainer greeting='Desafio Coder'/>
+        {/*  
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +24,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
     </div>
   );
